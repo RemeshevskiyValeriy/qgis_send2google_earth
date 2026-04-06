417d31714f414daac2456b6190b793488a6b1c80
Valeriy Remeshevskiy <remeshevskiy@gmail.com>
2026-04-06 17:25:30 +0300

    Update about dialog

--- src/send2google_earth/about_dialog.py
+++ src/send2google_earth/about_dialog.py
@@ -1,5 +1,5 @@
 # Send2GE Plugin
-# Copyright (C) 2025  NextGIS
+# Copyright (C) 2026  NextGIS
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -239,6 +239,7 @@ class AboutDialog(QDialog, Ui_AboutDialogBase):
             "main_url": main_url,
             "data_url": main_url.replace("://", "://data."),
             "get_involved_url": f"https://nextgis.com/redirect/{locale}/ak45prp5?{utm}",
+            "community_url": "https://community.nextgis.com",
             "utm": f"?{utm}",
             "speaks_russian": str(speaks_russian),
         }
@@ -251,6 +252,7 @@ class AboutDialog(QDialog, Ui_AboutDialogBase):
         titles = {
             "developers_title": self.tr("Developers"),
             "homepage_title": self.tr("Homepage"),
+            "community_title": self.tr("Join the community"),
             "user_guide": self.tr("User Guide"),
             "report_title": self.tr("Please report bugs at"),
             "report_end": report_end,
@@ -274,6 +276,7 @@ class AboutDialog(QDialog, Ui_AboutDialogBase):
         description += """
             <p><b>{developers_title}:</b> <a href="{main_url}/{utm}">{authors}</a></p>
             <p><b>{homepage_title}:</b> <a href="{homepage_url}">{homepage_url}</a></p>
+            <p><b>{community_title}:</b> <a href="{community_url}/{utm}">{community_url}</a></p>
             <p><b>{report_title}</b> <a href="{tracker_url}">{bugtracker_title}</a> {report_end}</p>
         """
 
